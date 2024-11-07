43b7c76a629baa782d6317f85f97e6da301b9220
Sawyer-Anderson1 <sawyerlynnanderson@icloud.com>
2024-11-07 13:47:02 -0600

    Update 03_Sawyer_Anderson.asm

--- 03_Sawyer_Anderson.asm
+++ 03_Sawyer_Anderson.asm
@@ -8,8 +8,9 @@
 # Data segment
 #-------------
 		.data
-string:		.asciiz "Enter an integer from 0 to 100:"
+string:		.asciiz "Enter an integer from 0 to 100: "
 errorString:	.asciiz "The number was invalid (not 0-100)"
+successString:	.asciiz "The sum of double integers from 0 to N is: "
 integer:	.word 0
 array:		.space 404 # allocates array space for the maximum input and array that the program allows
 index:		.word 0
@@ -47,14 +48,13 @@ main:
 	lw	$t0, integer # loading in the integer
 	la	$t1, array # loading the array
 	lw	$t2, index # loading the current index
+	addi	$s3, $t0, 1 #to make it be a while(index <= N) (i.e. N + 1)
+	# the index starts at 0 so it will loop N + 1 for the array that is N + 1 long
+	# the index will be also used for the values, since 0*2, 1*2, 2*2, 3*2,.., N*2 are the values for this array
 	
 _arrayLoop:
 
-	addi	$t3, $t0, 1 #to make it be a while(index <= N) (i.e. N + 1)
-	# the index starts at 0 so it will loop N + 1 for the array that is N + 1 long
-	# the index will be also used for the values, since 0*2, 1*2, 2*2, 3*2,.., N*2 are the values for this array
-
-	beq	$t2, $t3, _sumLoop
+	beq	$t2, $s3, sumFunction
 	
 	add	$t4, $t2, $t2 # to get the values
 	add	$t5, $t4, $t4 # to get the actual index position
@@ -66,7 +66,26 @@ _arrayLoop:
 
 	j	_arrayLoop # loop
 
-_sumLoop: 
+sumFunction: 
+	
+	la	$t1, array
+	li  	$t2, 0  # reset index to 0
+	addi	$s1, $s1, 0 # sum accumulator
+	
+	_sumLoop: 
+		
+		sll	$t3, $t2, 2
+		add	$t3, $t3, $t1 # to get the actual array index
+		
+		lw	$s2, 0($t3)
+		add	$s1, $s1, $s2
+		
+		addi	$t2, $t2, 1 # increment
+		
+		bne	$t2, $s3, _sumLoop
+		
+		sw	$s1, sum # storing the sum accumulator into sum
+		j	success
 
 error: 
 
@@ -74,6 +93,19 @@ error:
 	la	$a0, errorString
 	syscall
 
+	j	exit
+
+success: 
+
+	li	$v0, 4
+	la	$a0, successString
+	syscall
+	
+	# output the sum here
+	lw	$a0, sum
+	li	$v0, 1
+	syscall
+	
 	j	exit
 
 exit: 
